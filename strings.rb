d77e8398edfa6d971f23608da3a4869f196a2723
Hsing-Hui Hsu <hsing-hui@carezone.com>
2015-08-08 20:56:26 -0700

    Solved string problem 2

--- strings.rb
+++ strings.rb
@@ -1,5 +1,17 @@
 # 2. Given two strings, write a method to decide if one is a permutation of the other?
 
+def permutation?(str1, str2)
+  letter_counts(str1) == letter_counts(str2)
+end
+
+def letter_counts(str)
+  counts = Hash.new 0
+  str.each_char do |char|
+    counts[char] += 1
+  end
+  counts
+end
+
 # 6. Given two sequences, print longest common subsequence
 
 # LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
--- strings_spec.rb
+++ strings_spec.rb
@@ -3,19 +3,19 @@ require_relative "strings"
 describe "permutation?" do
   it "returns true if two strings are permuations of each other" do
     str1 = "abc"
-    str1 = "bca"
+    str2 = "bca"
     expect(permutation?(str1, str2)).to eq true
   end
 
   it "returns true if two strings are permuations of each other" do
     str1 = "abc"
-    str1 = "tca"
+    str2 = "tca"
     expect(permutation?(str1, str2)).to eq false
   end
 end
 
-decribe "longest_subsequence" do
-  it "returns the longest substring of both strings" do
+describe "longest_subsequence" do
+  xit "returns the longest substring of both strings" do
     str1 = "AGGTAB",
       str2="GXTXAYB"
     expect(output(str1, str2)).to eq "GTAB"
@@ -24,7 +24,7 @@ end
 
 describe "shortest_string" do
 
-  it "returns the shortest string with both substrings" do
+  xit "returns the shortest string with both substrings" do
     str1 = "geek"
     str2="eke"
     expect(output(str1, str2)).to eq "geeke"
